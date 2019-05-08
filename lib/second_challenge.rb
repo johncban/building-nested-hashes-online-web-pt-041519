a3ba7435d4b50817a1ecd9712bbe6f9d2dd81102
johncban <johncban@gmail.com>
2019-05-08 01:35:59 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,7 +2,7 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-        :partriarch => {},
+        :patriarch => {},
         :matriarch => {},
         :hero => {},
         :hero_friends => []
